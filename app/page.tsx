5899f9bb7f38680b987f6bd561f0aadfae40fa06
Ays1004 <yewaleayush@gmail.com>
2025-06-29 14:30:02 +0530

    added skeleton components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import About from "@/components/About/About";
+import Contact from "@/components/Contact/Contact";
+import FAQ from "@/components/FAQ/FAQ";
 import Hero from "@/components/Hero/Hero";
+import Services from "@/components/Services/Services";
 
 export default function Home() {
     return (
@@ -14,6 +17,22 @@ export default function Home() {
                     <About />
                 </div>
             </div>
+            <div className="flex items-center justify-center w-full h-full">
+                <div className="p-8 h-200 w-full flex items-center justify-center">
+                    <Services />
+                </div>
+            </div>
+            <div className="flex items-center justify-center w-full h-full">
+                <div className="p-8 h-200 w-full flex items-center justify-center">
+                    <FAQ />
+                </div>
+            </div>
+            <div className="flex items-center justify-center w-full h-full">
+                <div className="p-8 h-200 w-full flex items-center justify-center">
+                    <Contact />
+                </div>
+            </div>
+            
         </div>
     );
 }

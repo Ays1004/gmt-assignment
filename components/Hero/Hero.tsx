1830a388593f40eab4c4a81edaeccf0ecfbefb8e
Ays1004 <yewaleayush@gmail.com>
2025-06-30 18:14:56 +0530

    fixed the hero section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <div className="w-full bg-[#ecebe3] overflow-hidden">
       {/* full-bleed container */}
       <div
-        className="relative w-screen h-200 left-1/2 -translate-x-1/2 overflow-hidden "
+        className="relative w-full h-full md:h-[60vw] lg:h-[55vw] lg:max-h-[720px] left-1/2 -translate-x-1/2 overflow-hidden "
       >
         <video
           src="/videos/video.mp4"
@@ -18,24 +18,26 @@ const Hero = () => {
         />
 
         {/* no horizontal padding here, just full-height overlay */}
-        <div className="relative z-10 flex flex-col items-center justify-center w-full h-full bg-black/30">
-          <h1 className="text-xl font-semibold text-white text-center mb-4">
+        <div className="relative z-10 flex flex-col my-20 items-center justify-center w-full h-full">
+          <h1 className="text-5xl font-semibold text-white text-center mb-4 px-6 mt-20 ">
             Psychological Care for
-            <br />
+
+          </h1>
+          <h1 className="text-5xl/tight font-semibold text-white text-center mt-5 mb-4 px-6 tracking-wide">
             Change, Insight, and Well-Being
           </h1>
 
-          <p className="text-base text-white text-center mb-6 font-light max-w-xl">
+
+          <h1 className="text-2xl text-white text-center mb-6 px-6 font-extralight max-w-xl">
             Offering individual psychotherapy for adults via telehealth in Michigan and{" "}
             <span className="underline">most U.S. states</span>
             <br className="hidden sm:block" />
-            through PSYPACT participation
-          </p>
+            {" "}through PSYPACT participation
+          </h1>
 
           <button
-            className="px-8 py-4 bg-[#A6BFCB] text-white text-lg font-semibold rounded-full"
-          >
-            SCHEDULE A CONSULTATION
+            className="px-20 py-4 bg-[#84aeb1] hover:opacity-60 text-white text-md rounded-[50%_/_50%]"
+          > SCHEDULE A CONSULTATION
           </button>
         </div>
       </div>

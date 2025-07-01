7d29b07d94d228834a1f31920c7b414e5fa706cc
Ays1004 <yewaleayush@gmail.com>
2025-07-01 20:58:56 +0530

    made few microadjustments

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -12,7 +12,7 @@ const Header = () => {
         <header className="w-full flex items-center justify-between">
             <div className="flex items-center" onClick={handleClick}>
                 {/* Logo placeholder */}
-                <Appear>
+                <Appear delay={0} duration={0.4}>
                     <div className="flex items-center justify-center w-14 h-14 md:w-16 md:h-16 border border-[#B6C1B0] opacity-60 m-2 relative overflow-hidden">
                         <Image
                             src="/images/icon.png"
@@ -23,7 +23,7 @@ const Header = () => {
                         />
                     </div>
                 </Appear>
-                <Appear>
+                <Appear delay={0} duration={0.4}>
                     <div className="flex flex-col pr-2 justify-center">
                         <h1 className="text-xl text-[hsla(60,1.56%,25.1%,1)] leading-tight">
                             Dr. Serena Blake, PsyD
@@ -36,7 +36,9 @@ const Header = () => {
             </div>
 
             <div className="flex md:hidden items-center space-x-6">
-                <HamMenu />
+                <Appear delay={0} duration={0.4}>
+                    <HamMenu />
+                </Appear>
             </div>
         </header>
     );
